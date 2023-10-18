8c7b37226d1ed91f8ac8f46057d9c87c67b299d5
Pedro <pedrogoku80@gmail.com>
2023-10-17 23:42:30 -0300

    Busca Binaria

---
+++ cap1/buscaBinaria.js
@@ -0,0 +1,24 @@
+function buscaBinaria(list, key){
+    let inicio = 0;
+    let fim = list.length - 1;
+
+    while(inicio <= fim){
+        let meio = Math.floor((inicio+fim)/2);
+        let value = list[meio];
+
+        if(value === key){
+            return meio;
+        } else if(value > key){
+            fim = meio - 1;
+        }else {
+            inicio = meio + 1;
+        }
+    }
+
+    return -1;
+}
+
+const vet = [1,3,4,7,8,9];
+
+console.log(buscaBinaria(vet,4));
+console.log(buscaBinaria(vet,10));
